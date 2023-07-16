de3b30df3ca055b14e7b0270f6262e24e1d6bb6f
jojo <rouangchalern.phasouk11@gmail.com>
2023-07-16 14:42:38 +0700

    First verison

---
+++ JoProject/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+ <div class="d-flex flex-row mb-3">
+  <div class="p-2">ສະບາຍດີ</div>
+  <div lang="la">ສະບາຍດີ</div>
+  <div class="p-2">Flex item 3</div>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
